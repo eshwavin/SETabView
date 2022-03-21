f1a27033c562cfab8c8f6a0bc05c9c32251f1217
Srivinayak Chaitanya Eshwa <eshwavin@gmail.com>
2022-03-21 09:50:48 +0530

    Changed deprecated version. Bumped up version

--- SETabView.podspec
+++ SETabView.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name             = 'SETabView'
-  s.version          = '1.4.0'
+  s.version          = '2.0.0'
   s.summary          = 'SETabView is a custom TabBar with really cool animations'
 
   s.description      = <<-DESC
--- Source/Deprecated/Deprecated.swift
+++ Source/Deprecated/Deprecated.swift
@@ -5,16 +5,16 @@
 //  Created by Vinayak Eshwa on 20/03/22.
 //
 
-@available(iOS, obsoleted: 1.4.0, renamed: "SETabViewController")
+@available(iOS, obsoleted: 2.0.0, renamed: "SETabViewController")
 open class SEViewController { }
 
-@available(iOS, obsoleted: 1.4.0, renamed: "SETabItemProvider")
+@available(iOS, obsoleted: 2.0.0, renamed: "SETabItemProvider")
 public protocol SETabItem { }
 
 public extension SETabViewController {
-    @available(iOS, obsoleted: 1.4.0, message: "Use setTabColors(backgroundColor:ballColor:tintColor:unselectedItemTintColor:barTintColor:) instead")
+    @available(iOS, obsoleted: 2.0.0, message: "Use setTabColors(backgroundColor:ballColor:tintColor:unselectedItemTintColor:barTintColor:) instead")
     func setTabSettings(tabColor: UIColor, ballColor: UIColor, selectedTabTintColor: UIColor, deselectedTabTintColor: UIColor, animationDuration: Double) { }
     
-    @available(iOS, obsoleted: 1.4.0, message: "Use setViewControllers(_:) instead")
+    @available(iOS, obsoleted: 2.0.0, message: "Use setViewControllers(_:) instead")
     func setViewControllers(_ viewControllers: [UIViewController], initialSelectedIndex: Int, animationType: AnimationType) { }
 }
